c475b8160cfad1e6441519d60f79c41e44102860
cloud <congj.yang@hotmail.com>
2017-06-01 17:48:13 +0800

    update test

--- src/main/scala/org/wulfnoth/gadus/rpc/Main.scala
+++ src/main/scala/org/wulfnoth/gadus/rpc/Main.scala
@@ -20,33 +20,7 @@ object Main {
 
   def main(args: Array[String]): Unit = {
 
-    if (args.length < 3) {
-      help
-      System.exit(-1)
-    } else if (args(0) == "server") {
-      RPC.getServerBuilder.
-        instance(new TestRPCImpl).
-        address(new InetSocketAddress(args(1), args(2).toInt)).build.start
-    } else if (args(0) == "client") {
-      val testRPC = RPC.getProxy(classOf[TestRPC],
-        new InetSocketAddress(args(1), args(2).toInt))
-      var v1 = 0
-      var v2 = 0
-      0 until args(3).toInt foreach { x =>
-        v1 = Random.nextInt(10000000)
-        v2 = Random.nextInt(10000000)
-
-      }
-    }
   }
 
 }
 
-
-trait TestRPC {
-  def add(a: Int, b: Int): Int
-}
-
-class TestRPCImpl extends TestRPC {
-  override def add(a: Int, b: Int): Int = a+b
-}
--- src/test/scala/org/wulfnoth/gadus/rpc/RPCTest.scala
+++ src/test/scala/org/wulfnoth/gadus/rpc/RPCTest.scala
@@ -14,6 +14,8 @@ class RPCTest {
 	val server = RPC.getServerBuilder
 		.address(new InetSocketAddress("0.0.0.0", 9999)).instance(new TestRPCImpl).build
 
+	val count = 1
+
 	var startTime = 0L
 
 	@Before
@@ -26,12 +28,14 @@ class RPCTest {
 	def test(): Unit = {
 
 		val testRPC = RPC.getProxy(classOf[TestRPC], new InetSocketAddress("0.0.0.0", 9999))
-		0 until 1000000 foreach { x =>
-			val v1 = Random.nextInt(100000000)
-			val v2 = Random.nextInt(100000000)
+		0 until count foreach { x =>
+			val arr = Array(1,2,3,4,5,6,7,8,9)
+			/*val v1 = Random.nextInt(100000000)
+			val v2 = Random.nextInt(100000000)*/
 			//println(v1 + v2)
 			try {
-				assertEquals(v1+v2, testRPC.add(v1, v2))
+				assertEquals(testRPC.rd(arr), 45)
+				//assertEquals(v1+v2, testRPC.add(v1, v2))
 			} catch {
 				case t: Throwable =>
 					println(x)
@@ -44,15 +48,20 @@ class RPCTest {
 	@After
 	def close(): Unit = {
 		server.close()
-		println(System.currentTimeMillis - startTime)
+		val duration = (System.currentTimeMillis - startTime)/1000.0
+		printf("总耗时%.2f秒\n平均%.2f条/秒\n", duration, count/duration)
+		//println(String.format("总耗时%.2f\n平均%.2f条/秒", duration, count/duration))
+		//println(s"总耗时${duration}s\n平均${count/duration}条/秒")
 	}
 
 }
 
 trait TestRPC {
-	def add(a: Int, b: Int): Int
+	def rd(list: Array[Int]): Int
 }
 
 class TestRPCImpl extends TestRPC {
-	override def add(a: Int, b: Int): Int = a+b
+
+	def rd(list: Array[Int]): Int = list.sum
+
 }
